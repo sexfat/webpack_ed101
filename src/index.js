8c9a6d9852726f0e45631e9ec4c73869e9271e67
sexfat <bryant6831@gmail.com>
2020-06-03 15:25:49 +0800

    add html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // import  "./style.css";
 // import  "./header.css";
+var obj =  require('./cal');
 import  "./sass/style.scss";
 
-
-var obj =  require('./cal');
-
-console.log(obj.cal(10));
-console.log(obj.name);
+// console.log(obj.cal(10));
+// console.log(obj.name);
 // console.log('ok');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
-    mode: 'development', // "production" | "development" | "none"  開發模式
+    mode: 'production', // "production" | "development" | "none"  開發模式
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -32,11 +34,23 @@ module.exports = {
         }]
     },
     plugins: [
+         //清理舊的檔案
+         new CleanWebpackPlugin(),
         //這個套件是載入 css 檔案
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
             filename: "./css/[name].css"
+        }),
+        new HtmlWebpackPlugin({
+            title : '首頁',
+            //來源檔
+            template: './src/index.html',
+            //產生的檔案
+            filename: 'index.html', 
+            minify: false,
+            inject: 'body' ,
         })
+
     ]
    };
